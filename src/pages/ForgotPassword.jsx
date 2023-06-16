39338750e9bdb1f426835a693870d4cb54bed4b9
yogesh <hemant.kaushik@writlabs.com>
2023-06-16 11:43:12 +0530

    changes in ui

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 export default function ForgotPassword() {
-  return <div>ForgotPassword</div>;
+  const [email, setEmail] = useState("");
+
+  function onChange(e) {
+    setEmail((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold"> Forgot Password</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          {" "}
+          <img
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
+            alt="image"
+            className="w-full rounded-2xl"
+          ></img>
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              className=" mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email Address ..."
+            />
+            <div className="flex justify-between whitespace-nowrap">
+              <p className="mb-6">
+                Don't have a account?{" "}
+                <Link to="/sign-up" className="text-red-600 hover:text-red-700">
+                  Register
+                </Link>{" "}
+              </p>
+              <p>
+                <Link to={"/sign-in"} className="text-blue-500">
+                  {" "}
+                  Sign In Instead
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-7 py-3  text-sm font-medium uppercase 
+            rounded shadow-md hover:bg-blue-700 transition duration-150 ease-up-down hover:shadow-lg active:bg-blue-800"
+            >
+              Send Reset Password
+            </button>
+            <div
+              className="flex items-center my-4 before:border-t  before:flex-1  before:border-gray-300 
+          after:border-t  after:flex-1  after:border-gray-300"
+            >
+              <p className="text-center font-semibold mx-4 ">OR</p>
+            </div>
+            <OAuth />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
